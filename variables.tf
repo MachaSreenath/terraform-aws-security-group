ae3c66f3671fb2bd9a94b2096d5f3e3f9c18b017
MachaSreenath <sreenathshree9@gmail.com>
2025-01-26 10:13:21 +0530

    terraform aws security group changes

--- variables.tf
+++ variables.tf
@@ -1,28 +1,28 @@
 variable "project_name" {
-  
+
 }
 variable "environment" {
-  
+
 }
 variable "sg_name" {
   #default = mongodb 
 }
 variable "vpc_id" {
-  
+
 }
 variable "sg_tags" {
-    default = {}
-    type = map
+  default = {}
+  type    = map(any)
 }
 variable "common_tags" {
-    default = {}
-    type = map
+  default = {}
+  type    = map(any)
 }
 variable "sg_description" {
-    # default = ""
-    type = string
+  # default = ""
+  type = string
 }
 variable "sg_ingress_rules" {
-  type = list
+  type    = list(any)
   default = []
 }
